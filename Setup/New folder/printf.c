1f1bb26565f7efc963c0a7375d9459b5136e3bfd
Jami5826 <ar.jami.5826@gmail.com>
2023-04-02 00:56:27 +0600

    printf.c

---
+++ Setup/New folder/printf.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main()
+{
+     printf("hello world\n");
+     printf("My name is ar jami\n");
+     printf ("hello world\nMy name is AR JAMI\n");
+     printf("hello world    My name is AR JAMI\n");
+     printf("hello world\tMy name is AR JAMI");
+     return 0;
+}
+//  \n - new line, je jaigai noton line chai thik oi jaygay
+//  \t - tab mne 4 ghor space...
+//  hello world
+//  My name is ar jami

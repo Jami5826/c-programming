16545f2c2b53b429dca5544af2fa187718fe8499
Jami5826 <ar.jami.5826@gmail.com>
2023-04-01 16:40:13 +0600

    sum.c

--- Setup/sum.c
+++ Setup/sum.c
@@ -2,9 +2,7 @@
 int main()
 {
     int a, b, sum;
-    printf ("Enter the value of a");
     scanf ("%d",&a);
-    printf ("Enter the value of b");
     scanf ("%d",&b);
     sum = a + b;
     printf ("The sum is=%d", sum);

bcaba3d390343427a219286f968f950200c04431
Jami5826 <ar.jami.5826@gmail.com>
2023-04-04 15:42:32 +0600

    quiz_auestion.c

---
+++ ajaira code/quiz_question.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+    int x=0;
+    if (x)
+    // x=0 akhon ) mne hocche false r bki ja ase oigola hole true..
+    // tahole jinis ta amn if(0).. jeheto 0 mne false ..tai if er shortotao false..Tai computer ar if er bhitorer shorto cheak korbe nah.. direct else e chole jabe..
+    {
+        printf("hi");
+    }
+    else
+    {
+        printf("how are you?");
+    }
+    return 0;
+}
